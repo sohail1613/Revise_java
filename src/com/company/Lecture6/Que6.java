fdd11c263de38888884764bde92f8d6bcc1042c6
Your Name <sohail1613@gmail.com>
2022-01-17 06:38:42 +0530

    Solution modified

--- src/com/company/Lecture6/Que6.java
+++ src/com/company/Lecture6/Que6.java
@@ -1,2 +1,22 @@
-package com.company.Lecture6;public class Que6 {
+//Print inverted half pyramid.
+
+package com.company.Lecture6;
+
+import java.util.Scanner;
+
+public class Que6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to get pyramid :");
+        int number= sc.nextInt();
+
+        //outer loop
+        for (int i=1; i<=number; i++){
+            //inner loop
+            for (int j=i; j<=number; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
 }
