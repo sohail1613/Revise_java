30edf776d824471f732e3632b5b244b979d182cc
Your Name <sohail1613@gmail.com>
2022-01-17 15:19:04 +0530

    Rectify spelling

--- src/com/company/Lecture10/Que1.java
+++ src/com/company/Lecture10/Que1.java
@@ -19,7 +19,7 @@ public class Que1 {
 
         //output
         for (int i=0; i< names.length; i++){
-            System.out.println(names[i]+" ");
+            System.out.println("Name "+(i)+" is : "+names[i]);
         }
     }
 }
