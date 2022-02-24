f6ff60cf35db0542c83070d3a8f7fbc554241b5c
Sohail <sohail1613@gmail.com>
2022-02-24 22:25:34 +0530

    Files added

--- src/com/company/Lecture11/Que3.java
+++ src/com/company/Lecture11/Que3.java
@@ -1,2 +1,22 @@
-package com.company.Lecture11;public class Que3 {
+//Digits sum of an integer using function
+
+package com.company.Lecture11;
+
+import java.util.Scanner;
+
+public class Que3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int digit = sc.nextInt();
+        System.out.println("The sum of the number is: "+digitsSum(digit));
+    }
+
+    public static int digitsSum(int number){
+        int sum=0;
+        while (number!=0){
+            sum = sum+number%10;
+            number = number/10;
+        }
+        return sum;
+    }
 }
