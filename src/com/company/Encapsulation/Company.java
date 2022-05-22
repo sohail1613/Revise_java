4bc4af02cee34211db1b47bea3a5a81ad6146357
Sohail <sohail1613@gmail.com>
2022-05-22 17:30:41 +0530

    Encapsulation

---
+++ src/com/company/Encapsulation/Company.java
@@ -0,0 +1,34 @@
+package com.company.Encapsulation;
+//Encapsulation = Binding date with methods
+public class Company {
+    private int empId;
+    private String name;
+
+    //using setter and getter to access variables.
+    public void setEmpId(int empId){
+        this.empId = empId;
+    }
+    public int getEmpId(){
+        return empId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
+class Demo{
+    public static void main(String[] args) {
+        Company c = new Company();
+//        c.empId=232; //its not right way to access directly , we should access using getters and setters method
+//        c.name = "Adari";
+
+        c.setEmpId(123); //to set value
+        System.out.println(c.getEmpId()); //to fetch the value
+        c.setName("Sohail");
+        System.out.println(c.getName());
+    }
+}
---
+++ src/com/company/Encapsulation/Main.java
@@ -0,0 +1,6 @@
+package com.company.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
